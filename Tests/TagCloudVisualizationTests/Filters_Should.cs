c82e73a78a0ca42e4ad2a236f05e542145f91923
Tatiana Gladysheva <bububu241@gmail.com>
2017-12-24 15:38:57 +0500

    почти починила тесты

--- Tests/TagCloudVisualizationTests/Filters_Should.cs
+++ Tests/TagCloudVisualizationTests/Filters_Should.cs
@@ -1,96 +1,97 @@
-﻿//using System.Collections.Generic;
-//using System.Linq;
-//using FluentAssertions;
-//using Moq;
-//using NUnit.Framework;
-//using TagsCloudVisualization.WordAnalyzer;
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using TagsCloudVisualization;
+using TagsCloudVisualization.WordAnalyzer;
 
-//namespace Tests.TagCloudVisualizationTests
-//{
-//    [TestFixture]
-//    public class Filters_Should
-//    {
-//        private ShortWordsFilter shortWordsFilter;
-//        private PartOfSpeechFilter partsOfSpeechFilter;
-//        private DefaultBlackList blackList;
-//        [SetUp]
-//        public void SetUp()
-//        {
-//            shortWordsFilter = new ShortWordsFilter();  
-//            var fakePosTagger = new Mock<IPartsOfSpeechRecognizer>();
-//            fakePosTagger.Setup(m => m.Recognize(It.IsAny<string>())).Returns(PartsOfSpeech.Adjective);
-//            fakePosTagger.Setup(m => m.Recognize(It.Is<string>(v => v == "can"))).Returns(PartsOfSpeech.Verb);
-//            fakePosTagger.Setup(m => m.Recognize(It.Is<string>(v => v == "aa"))).Returns(PartsOfSpeech.Unknown);
-//            partsOfSpeechFilter = new PartOfSpeechFilter(new []{PartsOfSpeech.Verb}, fakePosTagger.Object);
-//            blackList = new DefaultBlackList(new []{"boredum"});
-//        }
+namespace Tests.TagCloudVisualizationTests
+{
+    [TestFixture]
+    public class Filters_Should
+    {
+        private ShortWordsFilter shortWordsFilter;
+        private PartOfSpeechFilter partsOfSpeechFilter;
+        private DefaultBlackList blackList;
+        [SetUp]
+        public void SetUp()
+        {
+            shortWordsFilter = new ShortWordsFilter();
+            var fakePosTagger = new Mock<IPartsOfSpeechRecognizer>();
+            fakePosTagger.Setup(m => m.Recognize(It.IsAny<string>())).Returns(Result.Ok(PartsOfSpeech.Adjective));
+            fakePosTagger.Setup(m => m.Recognize(It.Is<string>(v => v == "can"))).Returns(Result.Ok(PartsOfSpeech.Verb));
+            fakePosTagger.Setup(m => m.Recognize(It.Is<string>(v => v == "aa"))).Returns(Result.Ok(PartsOfSpeech.Unknown));
+            partsOfSpeechFilter = new PartOfSpeechFilter(new[] { PartsOfSpeech.Verb }, fakePosTagger.Object);
+            blackList = new DefaultBlackList(new[] { "boredum" });
+        }
 
-//        [Test]
-//        public  void ShortWordsFilterShuld_FulterWordShorter4_WhenWordsNotNull()
-//        {
-//            var words = new List<Word> {new Word("aa", 1), new Word("qwerty", 1)};
-//            var goodWords = shortWordsFilter.Filter(words).ToList();
-//            goodWords.Should().HaveCount(1);
-//            goodWords.ShouldBeEquivalentTo(new List<Word> { new Word("qwerty", 1) });
-//        }
+        [Test]
+        public void ShortWordsFilterShuld_FulterWordShorter4_WhenWordsNotNull()
+        {
+            var words = new List<Word> { new Word("aa", 1), new Word("qwerty", 1) };
+            var goodWords = shortWordsFilter.Filter(words).Value;
+            goodWords.Should().HaveCount(1);
+            goodWords.ShouldBeEquivalentTo(new List<Word> { new Word("qwerty", 1) });
+        }
 
-//        [Test]
-//        public void ShortWordsFilterShuld_FulterWordShorter4_WhenWordTextIsNull()
-//        {
-//            var words = new List<Word> { new Word(null, 1), new Word("qwerty", 1) };
-//            var goodWords = shortWordsFilter.Filter(words).ToList();
-//            goodWords.Should().HaveCount(1);
-//            goodWords.ShouldBeEquivalentTo(new List<Word> { new Word("qwerty", 1) });
-//        }
+        [Test]
+        public void ShortWordsFilterShuld_FulterWordShorter4_WhenWordTextIsNull()
+        {
+            var words = new List<Word> { new Word(null, 1), new Word("qwerty", 1) };
+            var goodWords = shortWordsFilter.Filter(words).Value;
+            goodWords.Should().HaveCount(1);
+            goodWords.ShouldBeEquivalentTo(new List<Word> { new Word("qwerty", 1) });
+        }
 
-//        [Test]
-//        public void ShortWordsFilterShuld_FulterWordShorter4_WhenWordIsNull()
-//        {
-//            var words = new List<Word> { null, new Word("qwerty", 1) };
-//            var goodWords = shortWordsFilter.Filter(words).ToList();
-//            goodWords.Should().HaveCount(1);
-//            goodWords.ShouldBeEquivalentTo(new List<Word> { new Word("qwerty", 1) });
-//        }
+        [Test]
+        public void ShortWordsFilterShuld_FulterWordShorter4_WhenWordIsNull()
+        {
+            var words = new List<Word> { null, new Word("qwerty", 1) };
+            var goodWords = shortWordsFilter.Filter(words).Value;
+            goodWords.Should().HaveCount(1);
+            goodWords.ShouldBeEquivalentTo(new List<Word> { new Word("qwerty", 1) });
+        }
 
-//        [Test]
-//        public void PartsOfSpeechFilterShould_FilterVerbs_WhenWordsIsNotNull()
-//        {
-//            var words = new List<Word> {new Word("can", 1), new Word("bla",1)};
-//            var goodWords = partsOfSpeechFilter.Filter(words).ToList();
-//            goodWords.Should().HaveCount(1);
-//            goodWords.ShouldBeEquivalentTo(new List<Word>{new Word("bla",1)});
-//        }
+        [Test]
+        public void PartsOfSpeechFilterShould_FilterVerbs_WhenWordsIsNotNull()
+        {
+            var words = new List<Word> { new Word("can", 1), new Word("bla", 1) };
+            var goodWords = partsOfSpeechFilter.Filter(words).Value;
+            goodWords.Should().HaveCount(1);
+            goodWords.ShouldBeEquivalentTo(new List<Word> { new Word("bla", 1) });
+        }
 
-//        [Test]
-//        public void PartsOfSpeechFilterShould_FilterVerbs_WhenWordTextIsNull()
-//        {
-//            var words = new List<Word> { new Word("can", 1), new Word(null, 1) };
-//            partsOfSpeechFilter.Filter(words).Should().HaveCount(0);
-//        }
+        [Test]
+        public void PartsOfSpeechFilterShould_FilterVerbs_WhenWordTextIsNull()
+        {
+            var words = new List<Word> { new Word("can", 1), new Word(null, 1) };
+            partsOfSpeechFilter.Filter(words).Value.Should().HaveCount(0);
+        }
 
-//        [Test]
-//        public void PartsOfSpeechFilterShould_FilterVerbs_WhenWordIsNull()
-//        {
-//            var words = new List<Word> { new Word("can", 1), null };
-//            partsOfSpeechFilter.Filter(words).Should().HaveCount(0);
-//        }
+        [Test]
+        public void PartsOfSpeechFilterShould_FilterVerbs_WhenWordIsNull()
+        {
+            var words = new List<Word> { new Word("can", 1), null };
+            partsOfSpeechFilter.Filter(words).Value.Should().HaveCount(0);
+        }
 
-//        [Test]
-//        public void PartsOfSpeechFilterShould_NotFilter_WhenWordIsUnknowPartOfSpeech()
-//        {
-//            var words = new List<Word> { new Word("aa", 1), new Word("blabla",1) };
-//            var goodWords = partsOfSpeechFilter.Filter(words).ToList();
-//            goodWords.Should().HaveCount(2);
-//            goodWords.ShouldBeEquivalentTo(words);
-//        }
+        [Test]
+        public void PartsOfSpeechFilterShould_NotFilter_WhenWordIsUnknowPartOfSpeech()
+        {
+            var words = new List<Word> { new Word("aa", 1), new Word("blabla", 1) };
+            var goodWords = partsOfSpeechFilter.Filter(words).Value;
+            goodWords.Should().HaveCount(2);
+            goodWords.ShouldBeEquivalentTo(words);
+        }
 
-//        [Test]
-//        public void BoringWordsFilterShould_FilterWordsFromBoringWordsList()
-//        {
-//            var words = new List<Word> { new Word("boredum", 1), new Word("blabla", 1) };
-//            var goodWords = blackList.Filter(words).ToList();
-//            goodWords.Should().HaveCount(1);
-//            goodWords.ShouldBeEquivalentTo(new List<Word>(){new Word("blabla",1)});
-//        }
-//    }
-//}
+        [Test]
+        public void BoringWordsFilterShould_FilterWordsFromBoringWordsList()
+        {
+            var words = new List<Word> { new Word("boredum", 1), new Word("blabla", 1) };
+            var goodWords = blackList.Filter(words).Value;
+            goodWords.Should().HaveCount(1);
+            goodWords.ShouldBeEquivalentTo(new List<Word>() { new Word("blabla", 1) });
+        }
+    }
+}
--- Tests/TagCloudVisualizationTests/WordsAnalyzer_Should.cs
+++ Tests/TagCloudVisualizationTests/WordsAnalyzer_Should.cs
@@ -1,93 +1,94 @@
-﻿//using System.Collections.Generic;
-//using System.Linq;
-//using FluentAssertions;
-//using Moq;
-//using NUnit.Framework;
-//using TagsCloudVisualization.WordAnalyzer;
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using TagsCloudVisualization;
+using TagsCloudVisualization.WordAnalyzer;
 
-//namespace Tests.TagCloudVisualizationTests
-//{
-//    [TestFixture]
-//    public class WordsAnalyzer_Should
-//    {
-//        private WordsAnalyzer analyzer;
-//        private Mock<IWordConverter> fakeConverter;
-//        private Mock<IWordsFilter> fakeFilter;
-//        private HashSet<WordsConverterType> fakeConvertesNames;
-//        private HashSet<FilterType> fakeFiltersNames;
-//        [SetUp]
-//        public void SetUp()
-//        {
+namespace Tests.TagCloudVisualizationTests
+{
+    [TestFixture]
+    public class WordsAnalyzer_Should
+    {
+        private WordsAnalyzer analyzer;
+        private Mock<IWordConverter> fakeConverter;
+        private Mock<IWordsFilter> fakeFilter;
+        private HashSet<WordsConverterType> fakeConvertesNames;
+        private HashSet<FilterType> fakeFiltersNames;
+        [SetUp]
+        public void SetUp()
+        {
 
-//            fakeFiltersNames = new HashSet<FilterType>() {FilterType.FilterShortWords};
-//            fakeConvertesNames = new HashSet<WordsConverterType>() {WordsConverterType.InitalFormConverter};
-//            fakeConverter = new Mock<IWordConverter>();
-//            fakeFilter = new Mock<IWordsFilter>();
-//            fakeFilter
-//                .SetupGet(m => m.Type)
-//                .Returns(FilterType.FilterShortWords);
-//            fakeFilter
-//                .Setup(m => m.Filter(It.IsAny<IEnumerable<Word>>()))
-//                .Returns<IEnumerable<Word>>(k => k);
-//            fakeConverter
-//                .SetupGet(m => m.Type)
-//                .Returns(WordsConverterType.InitalFormConverter);
-//            fakeConverter
-//                .Setup(m => m.Convert(It.IsAny<IEnumerable<string>>()))
-//                .Returns<IEnumerable<string>>(k => k);
-//            analyzer = new WordsAnalyzer(
-//                new List<IWordsFilter>() {fakeFilter.Object},
-//                new List<IWordConverter>() {fakeConverter.Object});
-//        }
+            fakeFiltersNames = new HashSet<FilterType>() { FilterType.FilterShortWords };
+            fakeConvertesNames = new HashSet<WordsConverterType>() { WordsConverterType.InitalFormConverter };
+            fakeConverter = new Mock<IWordConverter>();
+            fakeFilter = new Mock<IWordsFilter>();
+            fakeFilter
+                .SetupGet(m => m.Type)
+                .Returns(FilterType.FilterShortWords);
+            fakeFilter
+                .Setup(m => m.Filter(It.IsAny<IEnumerable<Word>>()))
+                .Returns<IEnumerable<Word>>(Result.Ok);
+            fakeConverter
+                .SetupGet(m => m.Type)
+                .Returns(WordsConverterType.InitalFormConverter);
+            fakeConverter
+                .Setup(m => m.Convert(It.IsAny<IEnumerable<string>>()))
+                .Returns<IEnumerable<string>>(Result.Ok);
+            analyzer = new WordsAnalyzer(
+                new List<IWordsFilter>() { fakeFilter.Object },
+                new List<IWordConverter>() { fakeConverter.Object });
+        }
 
-//        [Test]
-//        public void SettingsCallOne_WhenWordsCount1()
-//        {
-//            var words = new List<string>() { "blabla" };
-//            analyzer.Analyze(words, 1, fakeFiltersNames, fakeConvertesNames);
-//            fakeConverter.Verify(o => o.Convert(words), Times.Once);
-//            fakeFilter
-//                .Verify(o => o.Filter(It.Is<IEnumerable<Word>>(w => w.Count()==1 && w.First().Text=="blabla" && w.First().CountOfOccurrences==1)),
-//                Times.Once);
-//        }
+        [Test]
+        public void SettingsCallOne_WhenWordsCount1()
+        {
+            var words = new List<string>() { "blabla" };
+            analyzer.Analyze(words, 1, fakeFiltersNames, fakeConvertesNames);
+            fakeConverter.Verify(o => o.Convert(words), Times.Once);
+            fakeFilter
+                .Verify(o => o.Filter(It.Is<IEnumerable<Word>>(w => w.Count() == 1 && w.First().Text == "blabla" && w.First().CountOfOccurrences == 1)),
+                Times.Once);
+        }
 
-//        [Test]
-//        public void ExcludeShortWords_WhenWordsNotInBlackList()
-//        {
-//            fakeFilter
-//                .Setup(o => o.Filter(It.IsAny<IEnumerable<Word>>()))
-//                .Returns<IEnumerable<Word>>(w => w.Where(wd=> wd.Text.Length > 3));
-//            var words = new List<string> { "aa", "aa", "a" };
-//            analyzer.Analyze(words, 150, fakeFiltersNames, fakeConvertesNames).SortedWordsTop.Should().BeEmpty();
-//        }
+        [Test]
+        public void ExcludeShortWords_WhenWordsNotInBlackList()
+        {
+            fakeFilter
+                .Setup(o => o.Filter(It.IsAny<IEnumerable<Word>>()))
+                .Returns<IEnumerable<Word>>(w => Result.Ok(w.Where(wd => wd.Text.Length > 3)));
+            var words = new List<string> { "aa", "aa", "a" };
+            analyzer.Analyze(words, 150, fakeFiltersNames, fakeConvertesNames).Value.SortedWordsTop.Should().BeEmpty();
+        }
 
-//        [Test]
-//        public void RetutnFirst2Words_WhenWordsCount2()
-//        {
-//            var words = new List<string> { "acrosss", "acrosss", "acrosss", "aboutt", "aboutt", "abutt" };
-//            var actualTopWords = analyzer.Analyze(words, 2, fakeFiltersNames,fakeConvertesNames).SortedWordsTop;
-//            var expectedTop = new List<Word> { new Word("acrosss", 3), new Word("aboutt", 2) };
-//            actualTopWords.ShouldBeEquivalentTo(expectedTop);
-//        }
+        [Test]
+        public void RetutnFirst2Words_WhenWordsCount2()
+        {
+            var words = new List<string> { "acrosss", "acrosss", "acrosss", "aboutt", "aboutt", "abutt" };
+            var actualTopWords = analyzer.Analyze(words, 2, fakeFiltersNames, fakeConvertesNames).Value.SortedWordsTop;
+            var expectedTop = new List<Word> { new Word("acrosss", 3), new Word("aboutt", 2) };
+            actualTopWords.ShouldBeEquivalentTo(expectedTop);
+        }
 
-//        [Test]
-//        public void ReturnWordsInDescendingOrder()
-//        {
-//            var words = new List<string>()
-//            {
-//                "acrosss",
-//                "acrosss",
-//                "aboutt",
-//                "aboutt",
-//                "aboutt",
-//                "aboutt",
-//                "aboutt",
-//                "abutt"
-//            };
-//            var actualSortedWords = analyzer.Analyze(words, 4, fakeFiltersNames, fakeConvertesNames).SortedWordsTop;
-//            var expectedSortedWords =
-//                new List<Word>() { new Word("aboutt", 5), new Word("acrosss", 2), new Word("abutt", 1) };
-//            actualSortedWords.ShouldBeEquivalentTo(expectedSortedWords);
-//        }
-//    }
-//}
+        [Test]
+        public void ReturnWordsInDescendingOrder()
+        {
+            var words = new List<string>()
+            {
+                "acrosss",
+                "acrosss",
+                "aboutt",
+                "aboutt",
+                "aboutt",
+                "aboutt",
+                "aboutt",
+                "abutt"
+            };
+            var actualSortedWords = analyzer.Analyze(words, 4, fakeFiltersNames, fakeConvertesNames).Value.SortedWordsTop;
+            var expectedSortedWords =
+                new List<Word>() { new Word("aboutt", 5), new Word("acrosss", 2), new Word("abutt", 1) };
+            actualSortedWords.ShouldBeEquivalentTo(expectedSortedWords);
+        }
+    }
+}
